58df51950e1e8d4eaedae4e3e0ae5cf6306835d7
User4 <paper@LABPC04.mtaloy.lab>
2024-10-17 13:51:43 -0400

    Login

---
+++ src/home/components/ctf/form/LoginEntry.jsx
@@ -0,0 +1,13 @@
+const LoginEntry =() => {
+    return(
+        <div className="flex items-center justify-center border-2 border-white">
+            <form className="border border-yellow-600">
+                <p className="text-white">Username</p>
+                <input type="text" />
+                <p className="text-white">Password</p>
+                <input type="text" className="" />
+            </form>
+        </div>
+    )
+}
+export default LoginEntry
--- src/home/components/ctf/second/SecondContent.jsx
+++ src/home/components/ctf/second/SecondContent.jsx
@@ -1,10 +1,12 @@
+import LoginEntry from "../form/LoginEntry"
+
 const SecondContent =() => {
     return(
-        <div className="flex items-center pt-[120px] justify-between relative bg-black border border-yellow-500">
-            <div className="">
+        <div className="flex items-center pt-[120px] relative bg-black border border-yellow-500">
+            <div className="border-2 border-red-600">
                 <img src="/skeleton.jpg" className="object-contain w-[1200px] h-[1000px]" />
             </div>
-            <div className="absolute inset-0 backdrop-opacity-10 bg-black/30 z-30"></div>
+            <LoginEntry />
 
         </div>
     )
